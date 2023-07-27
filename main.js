21087c5482d772cbb0262814381950bc5b7fb5c7
TheMORTALTV11 <99326105+TheMORTALTV11@users.noreply.github.com>
2023-07-27 19:45:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ canvas=createCanvas(640,480)
 canvas.position(110,250);
 video=createCapture(VIDEO);
 video.hide();
-tint_color="";
+video.size(350,200);
 
 }
 function draw( ){
 image(video,0,0,640,480);
-tint(tint_color);scroll
+fill("red");
+stroke("red");
+circle(50,50,80);
+circle(590,50,80);
+circle(50,470,80);
+circle(590,470,80);
+fill("green");
+stroke("green");
+rect(30,90,40,340);
+rect(570,90,40,340);
+rect(80,30,470,40);
+rect(80,450,470,40);
 }
 function take_snapshot(){
     save('student_name.png');
-
-}
-function filter_tint()
-{
-    tint_color=document.getElementById("color_name").value;
 
 }
